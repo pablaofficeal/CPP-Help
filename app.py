2bda2e9ecd1219aed5d77ce169465942e4b30c25
Pavlo Gorosko <pavlogorosko56@gmail.com>
2025-06-10 20:22:44 +0200

    s

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 from flask_session import Session
-from flask_login import LoginManager
 import logging
 from logging import basicConfig, DEBUG
 from logging.handlers import RotatingFileHandler
-from models import *
+from models import db, User, Post
 from config import Config
 
 basicConfig(level=DEBUG, filename='app.log', filemode='w', format='%(asctime)s %(levelname)s %(message)s')
@@ -16,16 +15,15 @@ basicConfig(handlers=[file_handler])
 app = Flask(__name__)
 app.config.from_object(Config)
 
-Session(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'       
+Session(app) 
+db.init_app(app)
 
 with app.app_context():
     db.create_all()    
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=6457, debug=False)
