a632773801569a1a667aaff0033d1231e8951ad0
Pavlo Gorosko <pavlogorosko56@gmail.com>
2025-06-10 18:07:38 +0200

    `Added .env and config.py to .gitignore, updated config.py with unchanged content`

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'secret'
-    SESSION_TYPE = 'filesystem'
+    SESSION_TYPE = 'filesystem' 
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
+    
